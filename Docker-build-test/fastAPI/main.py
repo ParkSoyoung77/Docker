44c7e331a0cd0de1432ea9ec770840112c53976c
ParkSoyoung77 <39236790+ParkSoyoung77@users.noreply.github.com>
2026-03-06 12:03:05 +0900

    Add files via upload

---
+++ Docker-build-test/fastAPI/main.py
@@ -0,0 +1,9 @@
+import fatapi, uvicorn
+
+app = fastapi.FastAPI()
+@app.get("/")
+async def read_root():
+    return {"message": "Hellom Workld!"}
+
+# if __nama__ == "__main__":
+#       uvicorn.run("main:app", host='0.0.0.0', port=8000m reload=True)
